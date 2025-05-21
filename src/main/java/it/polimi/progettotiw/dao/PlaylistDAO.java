21b2cfebc0edfed9eaa2907d7a82a56e0c9f1de7
gd <giuseppe.dambrosi@mail.polimi.it>
2025-05-21 16:45:39 +0200

    reorder playlist

--- src/main/java/it/polimi/progettotiw/dao/PlaylistDAO.java
+++ src/main/java/it/polimi/progettotiw/dao/PlaylistDAO.java
@@ -113,7 +113,7 @@ public class PlaylistDAO {
     public void updateTracksOrder(int playlistId, List<Integer> orderedTrackIds) throws SQLException {
         String sql = "UPDATE Playlist_Tracks SET position = ? WHERE playlist_id = ? AND track_id = ?";
         try (PreparedStatement ps = con.prepareStatement(sql)) {
-            for (int i = orderedTrackIds.size(); i >=0; i--) {
+            for (int i = orderedTrackIds.size()-1; i >0; i--) {
                 ps.setInt(1, i - 1);
                 ps.setInt(2, playlistId);
                 ps.setInt(3, orderedTrackIds.get(i));
